e534db7c7cdf711b082469f025d7a01c574f47a4
AliRezahassnloo2731 <reza1632731@gmail.com>
2025-07-26 12:28:46 +0200

    Add Header,Loder,Error and Datecounter components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="logo512.jpg"
+        alt="logo"
+      />
+      <h1>The Quiza app</h1>
+    </header>
+  );
+}
+
+export default Header;
